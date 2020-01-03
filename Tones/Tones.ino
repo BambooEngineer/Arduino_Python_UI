28815bbb8879231c89b4086ebee3b6c13b694911
BambooEngineer <37255295+BambooEngineer@users.noreply.github.com>
2020-01-03 11:55:07 -0500

    Add files via upload

---
+++ Tones/Tones.ino
@@ -0,0 +1,36 @@
+char incomingByte; // for incoming serial data
+
+void setup() {
+  Serial.begin(115200); // opens serial port
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+  incomingByte = Serial.read();
+  
+  if(incomingByte == 'S'){
+    tone(2, 5, 1000);
+    
+  }
+  if(incomingByte == 'A'){
+    tone(2, 600, 1000);
+    
+  }
+  if(incomingByte == 'T'){
+    tone(2, 700, 1000);
+    
+  }
+  if(incomingByte == 'U'){
+    tone(2, 900, 1000);
+    
+  }
+  if(incomingByte == 'R'){
+    tone(2, 1500, 1000);
+   
+  }
+  if(incomingByte == 'N'){
+    tone(2,30, 1000);
+    
+  }
+  
+}
